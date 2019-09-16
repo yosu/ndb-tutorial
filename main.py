8854cf3d8e6b4e1e52ca49f062d351cdaecafe75
yosu <woodstock830@gmail.com>
2019-09-16 22:09:23 +0900

    Remove unused MainPage handler

--- main.py
+++ main.py
@@ -21,9 +21,6 @@ For more information, see README.md
 """
 
 # [START all]
-import cgi
-import textwrap
-import urllib
 import os
 
 from google.appengine.ext import ndb
@@ -47,56 +44,14 @@ class Book(ndb.Model):
         return cls.query().order(cls.name).fetch(limit)
 
 
-# [START greeting]
 class Greeting(ndb.Model):
     """Models an individual Guestbook entry with content and date."""
     content = ndb.StringProperty()
     date = ndb.DateTimeProperty(auto_now_add=True)
-# [END greeting]
 
-# [START query]
     @classmethod
     def query_book(cls, ancestor_key):
         return cls.query(ancestor=ancestor_key).order(-cls.date)
-
-
-class MainPage(webapp2.RequestHandler):
-    def get(self):
-        self.response.out.write('<html><body>')
-        guestbook_name = self.request.get('guestbook_name')
-        ancestor_key = ndb.Key("Book", guestbook_name or "*notitle*")
-        greetings = Greeting.query_book(ancestor_key).fetch(20)
-# [END query]
-
-        greeting_blockquotes = []
-        for greeting in greetings:
-            greeting_blockquotes.append(
-                '<blockquote>%s</blockquote>' % cgi.escape(greeting.content))
-
-        self.response.out.write(textwrap.dedent("""\
-            <html>
-              <body>
-                {blockquotes}
-                <form action="/sign?{sign}" method="post">
-                  <div>
-                    <textarea name="content" rows="3" cols="60">
-                    </textarea>
-                  </div>
-                  <div>
-                    <input type="submit" value="Sign Guestbook">
-                  </div>
-                </form>
-                <hr>
-                <form>
-                  Guestbook name:
-                    <input value="{guestbook_name}" name="guestbook_name">
-                    <input type="submit" value="switch">
-                </form>
-              </body>
-            </html>""").format(
-                blockquotes='\n'.join(greeting_blockquotes),
-                sign=urllib.urlencode({'guestbook_name': guestbook_name}),
-                guestbook_name=cgi.escape(guestbook_name)))
 
 
 class SubmitForm(webapp2.RequestHandler):
